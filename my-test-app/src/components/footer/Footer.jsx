d64c5f38e2dc47c88c3856a74bf8f15e5b125700
mbliok <mbliok@melontech.com>
2021-01-22 11:51:19 +0200

    moddules.css

--- my-test-app/src/components/footer/Footer.jsx
+++ my-test-app/src/components/footer/Footer.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import Button from '../Button';
+import styles from '../../styles/error.module.scss';
 
-function Footer(){
+function Footer() {
   return (
     <div className="app-footer">
-     <span>Customer service:  +395 898 000 555</span>
+      <Button text="footer" className="btn" />
+      <span className="error">Error css</span><br />
+      <span className={styles.error}>Error module</span><br />
+      <span>Customer service:  +395 898 000 555</span>
     </div>
   );
 }
--- my-test-app/src/components/header/Header.jsx
+++ my-test-app/src/components/header/Header.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import logo from '../../assets/img/logo.png';
-
+import Button from '../Button';
+import styles from '../../styles/button.module.scss';
 const Header = () => (
 
   <header className="app-header" >
+
+
+    <Button text="error" className={styles.btnError} />
+    <Button text="primary" className={styles.primary} />
     <img src={logo} alt="logo" className="app-logo" data-test="logo" />
 
     <span className="header-text">
